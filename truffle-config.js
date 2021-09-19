dd8a905374d01d086361cd9cc4726e05b6cc4604
alfredo-baron-mex <72430611+alfredo-baron-mex@users.noreply.github.com>
2021-09-19 10:23:48 -0500

    Update truffle-config.js
    Change to use Ganache-cli (port 8545)

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545, // Ganache-GUI 7545, Ganache-cli 8545
       network_id: "*" // Match any network id
     },
   },
